7583360decdf2c9589433edbc9474ed75eb46dce
Pratik Soni <pratikabu@gmail.com>
2012-10-31 02:00:06 +0530

    tweaked some resources

---
+++ main_website/extensions/scrolltotop/safaridownload.php
@@ -0,0 +1,6 @@
+<?php
+	// Permanent redirection
+	header("HTTP/1.1 301 Moved Permanently");
+	header("Location: https://github.com/downloads/pratikabu/scrolltotop/scroll-to-top-2.5.safariextz");
+	exit();
+?>
