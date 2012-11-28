51b55fa810b16ae53b11de7e98dabfc1439a3013
Pratik Soni <pratikabu@gmail.com>
2012-11-28 19:02:56 +0530

    donate

---
+++ main_website/extensions/scrolltotop/donate.php
@@ -0,0 +1,6 @@
+<?php
+	// Permanent redirection
+	header("HTTP/1.1 301 Moved Permanently");
+	header("Location: https://addons.mozilla.org/en-US/firefox/addon/scroll-to-top/developers");
+	exit();
+?>
