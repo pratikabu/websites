4793152ab2dbd55a9e8076b9930646b51c073c1a
Pratik Soni <pratikabu@gmail.com>
2012-11-29 14:54:24 +0530

    base64help completed

--- main_website/extensions/scrolltotop/safaridownload.php
+++ main_website/extensions/scrolltotop/safaridownload.php
@@ -1,6 +1,6 @@
 <?php
 	// Permanent redirection
 	header("HTTP/1.1 301 Moved Permanently");
-	header("Location: https://github.com/downloads/pratikabu/scrolltotop/scroll-to-top-3.0.safariextz");
+	header("Location: https://github.com/downloads/pratikabu/scrolltotop/scroll-to-top-3.6.safariextz");
 	exit();
 ?>
