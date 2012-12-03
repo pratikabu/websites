e6225ad7254e049123eeb5dabc389b6b1379501a
Pratik Soni <pratikabu@gmail.com>
2012-12-03 16:02:56 +0530

    Donate Page now captures GA.
    Google Analytics is now captured by donate page.

--- main_website/extensions/scrolltotop/donate.php
+++ main_website/extensions/scrolltotop/donate.php
@@ -1,6 +1,41 @@
-<?php
-	// Permanent redirection
-	header("HTTP/1.1 301 Moved Permanently");
-	header("Location: https://addons.mozilla.org/en-US/firefox/addon/scroll-to-top/developers");
-	exit();
-?>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+"http://www.w3.org/TR/html4/loose.dtd">
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+		<meta http-equiv="refresh" content="5;URL='http://addons.mozilla.org/en-US/firefox/addon/scroll-to-top/developers'">
+		<title>Thank You - Pratik Soni</title>
+		<link rel="shortcut icon" href="../../res/favicon.ico">
+		<link rel="stylesheet" href="../../res/stylesheet.css">
+		<link rel="stylesheet" href="res/stt.css">
+		
+		<script type="text/javascript">
+
+			var _gaq = _gaq || [];
+			_gaq.push(['_setAccount', 'UA-10242165-9']);
+			_gaq.push(['_trackPageview']);
+
+			(function() {
+			var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+			ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+			var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+			})();
+
+		</script>
+	</head>
+	
+	<body class="zeroMargin">
+		<div id="mainDiv" align="center" class="mainContent" style="width: 100%; padding-top: 10px; margin-top: 20px;">
+			<div align="center" class="tableClass documentContent" style="width: 270px;">
+				<div style="margin-bottom: 15px; margin-top: 15px;">
+					Thank you.<br/>
+					Redirecting to <a href="http://addons.mozilla.org/en-US/firefox/addon/scroll-to-top/developers">donation page</a>.
+				</div>
+				<div style="margin-bottom: 15px;"><img src="res/loader.gif" /></div>
+				<div style="margin-bottom: 15px; margin-top: 15px; font-size: 10px;">
+					<a href="http://addons.mozilla.org/en-US/firefox/addon/scroll-to-top/developers">Click here</a> if it does not redirects within 5 seconds.
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
