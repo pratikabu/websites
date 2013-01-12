7bbb639aa600437371128ca88ffb6bf2da111113
Pratik Soni <pratikabu@gmail.com>
2013-01-12 23:06:41 +0530

    Meta Info and link changes

--- main_website/files/coverletter_downloady.php
+++ main_website/files/coverletter_downloady.php
@@ -1,6 +1,6 @@
 <?php
 	// Permanent redirection
 	header("HTTP/1.1 301 Moved Permanently");
-	header("Location: https://github.com/downloads/pratikabu/websites/coverLetter_090512.pdf");
+	header("Location: http://goo.gl/qT1sL");
 	exit();
 ?>
