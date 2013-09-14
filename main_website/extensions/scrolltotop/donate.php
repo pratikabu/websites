453008f997f5a081c83f7f72321381d7e00a52a4
Pratik Soni <pratikabu@gmail.com>
2013-09-14 16:19:25 +0530

    Paypal addition for other browsers.

--- main_website/extensions/scrolltotop/donate.php
+++ main_website/extensions/scrolltotop/donate.php
@@ -1,9 +1,47 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
 "http://www.w3.org/TR/html4/loose.dtd">
+<?php
+	$paypalLink = "https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=pratikabu@gmail.com&currency_code=USD&item_name=Contribution%20for%20Scroll%20To%20Top.";
+	$mozillaLink = "http://addons.mozilla.org/en-US/firefox/addon/scroll-to-top/developers";
+	$donateUrl = $paypalLink;
+	$paypalName = "PayPal";
+	$mozillaName = "Mozilla";
+	$firefoxFound = isset($_SERVER['HTTP_USER_AGENT']) && strlen(strstr($_SERVER['HTTP_USER_AGENT'],"Firefox"));
+	if($firefoxFound > 0) {
+		$donateUrl = $mozillaLink;
+	}
+	
+	function printAltUrl() {
+		global $firefoxFound, $paypalLink, $mozillaLink;
+		if($firefoxFound) {
+			print_r($paypalLink);
+		} else {
+			print_r($mozillaLink);
+		}
+	}
+	
+	function printAltName() {
+		global $firefoxFound, $paypalName, $mozillaName;
+		if($firefoxFound) {
+			print_r($paypalName);
+		} else {
+			print_r($mozillaName);
+		}
+	}
+	
+	function printDonationName() {
+		global $firefoxFound, $paypalName, $mozillaName;
+		if($firefoxFound) {
+			print_r($mozillaName);
+		} else {
+			print_r($paypalName);
+		}
+	}
+?>
 <html>
 	<head>
 		<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
-		<meta http-equiv="refresh" content="5;URL='http://addons.mozilla.org/en-US/firefox/addon/scroll-to-top/developers'">
+		<meta http-equiv="refresh" content="5;URL='<?php print_r($donateUrl); ?>'">
 		<title>Thank You - Pratik Soni</title>
 		<link rel="shortcut icon" href="../../res/favicon.ico">
 		<link rel="stylesheet" href="../../res/stylesheet.css">
@@ -26,14 +64,17 @@
 	
 	<body class="zeroMargin">
 		<div id="mainDiv" align="center" class="mainContent" style="width: 100%; padding-top: 10px; margin-top: 20px;">
-			<div align="center" class="tableClass documentContent" style="width: 270px;">
+			<div align="center" class="tableClass documentContent" style="width: 350px;">
 				<div style="margin-bottom: 15px; margin-top: 15px;">
 					Thank you.<br/>
-					Redirecting to <a href="http://addons.mozilla.org/en-US/firefox/addon/scroll-to-top/developers">donation page</a>.
+					Redirecting to <a href="<?php print_r($donateUrl); ?>">donation page</a> using <?php printDonationName(); ?>.
 				</div>
 				<div style="margin-bottom: 15px;"><img src="res/loader.gif" /></div>
 				<div style="margin-bottom: 15px; margin-top: 15px; font-size: 10px;">
-					<a href="http://addons.mozilla.org/en-US/firefox/addon/scroll-to-top/developers">Click here</a> if it does not redirects within 5 seconds.
+					<a href="<?php print_r($donateUrl); ?>">Click here</a> if it does not redirects within 5 seconds.
+				</div>
+				<div style="margin-bottom: 15px; margin-top: 15px;">
+					Alternatively donate using <a href="<?php printAltUrl(); ?>"><?php printAltName(); ?></a>.
 				</div>
 			</div>
 		</div>
